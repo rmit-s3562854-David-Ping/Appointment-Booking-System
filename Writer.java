2ec3fedcddae8ae6e2e1e749fa32f9e2ffe3dd28
s3627014 <s3627014@student.rmit.edu.au>
2017-03-01 22:57:44 +1100

    Added Writer class
    Writer class, still needs to be tweaked to work properly with Daves files.

---
+++ Writer.java
@@ -0,0 +1,30 @@
+package writer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import members.Customers;
+
+public class Writer {
+
+	public void save(ArrayList<Customers> customers) throws IOException {
+		// Save holding array to holdings.txt and holdings_backup.txt via
+		// BufferedWriter.
+		BufferedWriter writerCustomers = new BufferedWriter(new FileWriter("customers.txt"));
+		int i = 0;
+		// A loop that writes each element of the array line by line to both
+		// files.
+		while (i < customers.size()) {
+			// Write to holdings.txt
+			writerCustomers.write(customers.get(i).toString());
+			writerCustomers.newLine();
+			// write to holdings_backup.txt
+			i++;
+			writerCustomers.close();
+			System.out.println("Files saved.");
+		}
+
+	}
+
+}
