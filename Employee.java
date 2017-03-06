481e43874db700b34b04a6d78c55a5ee73cd16e6
s3627014 <s3627014@student.rmit.edu.au>
2017-03-06 20:07:54 +1100

    Created Employee class with variables from class diagram.

---
+++ Employee.java
@@ -0,0 +1,11 @@
+package main;
+import java.time.LocalTime;
+/**
+ * Created by luke on 6/03/2017.
+ */
+public class Employee {
+    private String name;
+    private String id;
+    private LocalTime workingTimes;
+
+}
