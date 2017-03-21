cd6dac208e418c2ce31e5cddfa81f931736767c5
rmit-s3599135-hassenmender <hassen@DaLap>
2017-03-21 16:51:17 +1100

    try

---
+++ src/main/Employee.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Employee {
+
+	private static ArrayList<LocalDateTime> startTimes = new ArrayList<LocalDateTime>();// Change
+																						// to
+																						// next
+																						// month
+	private static ArrayList<LocalDateTime> endTimes = new ArrayList<LocalDateTime>();
+
+	private String id;
+	
+
+	public Employee(String firstName, String lastName, String id) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Employee() {
+	}
+
+	public ArrayList<LocalDateTime> getStartTimes() {
+		return startTimes;
+	}
+
+	public ArrayList<LocalDateTime> getEndTimes() {
+		return endTimes;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+}
