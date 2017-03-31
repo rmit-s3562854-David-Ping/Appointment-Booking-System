1800f0a1d73f34bee29f6a792632c77412f2b24c
s3627014 <s3627014@student.rmit.edu.au>
2017-03-31 13:33:32 +1100

    Auto generate ID for employee
    Created the function, and edited the addEmployee method in Owner

--- src/main/Employee.java
+++ src/main/Employee.java
@@ -12,8 +12,8 @@ public class Employee {
 	private String firstName;
 	private String lastName;
 
-	public Employee(String firstName, String lastName, String id, ArrayList<LocalDateTime> startTimes, ArrayList<LocalDateTime> endTimes) {
-		this.id = id;
+	public Employee(String firstName, String lastName, ArrayList<LocalDateTime> startTimes, ArrayList<LocalDateTime> endTimes) {
+		this.id = createID();
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
@@ -40,5 +40,35 @@ public class Employee {
 	public String getLastName() {
 		return lastName;
 	}
+
+
+	public  String createID()
+	{
+		//Sets ID to 0 then searches array, if ID exists it iterates and repeats.
+		Owner owner= new Owner();
+
+		ArrayList<Employee> EmployeeArray = owner.getEmployeeArray();
+		int IDCounter = 1;
+		int index = 0;
+		System.out.println(owner.getEmployeeArray().size());
+		while(index < EmployeeArray.size()) {
+			index=0;
+			while (index < EmployeeArray.size()) {
+				if (Integer.parseInt(String.format("%05d", IDCounter)) == Integer.parseInt((EmployeeArray.get(index).getId()).substring(1,6))) {
+					IDCounter++;
+					break;
+				} else {
+					index++;
+
+				}
+			}
+		}
+		String ID = "e" + String.format("%05d", IDCounter);
+		return ID;
+	}
+
+
+
+
 
 }
