f6d7f7ae9b368559ab5ff82c9c5af7cf84b8cbaa
rmit-s3562854-David-Ping <s3562854@student.rmit.edu.au>
2017-04-04 11:29:52 +1000

    Add files via upload

---
+++ src/JunitTest/ValidateBookingSummary_Test.java
@@ -0,0 +1,58 @@
+package JunitTest;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import main.*;
+
+public class ValidateBookingSummary_Test {
+
+	//Tests if date being considered for booking summary is before the current date and up to 1 month behind
+	//Used boundary checking
+	
+	//Checks if the current date returns a false
+	@Test
+	public void test() {
+		Owner owner = new Owner();
+		assertFalse(owner.validateBookingSummaryDate(LocalDate.now()));
+	}
+	
+	//Checks if the day after the current date returns a false
+	@Test
+	public void test1() {
+		Owner owner = new Owner();
+		assertFalse(owner.validateBookingSummaryDate(LocalDate.now().plusDays(1)));
+	}
+	
+	//Checks if the day before the current date is true (it is before current date)
+	@Test
+	public void test2() {
+		Owner owner = new Owner();
+		assertTrue(owner.validateBookingSummaryDate(LocalDate.now().minusDays(1)));
+	}
+	
+	//Checks if the date 1 month ago is valid, should return true
+	@Test
+	public void test3() {
+		Owner owner = new Owner();
+		assertTrue(owner.validateBookingSummaryDate(LocalDate.now().minusMonths(1)));
+	}
+	
+	//Checks if the date 1 month plus 1 day is true
+	@Test
+	public void test5() {
+		Owner owner = new Owner();
+		assertTrue(owner.validateBookingSummaryDate(LocalDate.now().minusMonths(1).plusDays(1)));
+	}
+	
+	//Checks if the date 1 month minus 1 day is false
+	@Test
+	public void test6() {
+		Owner owner = new Owner();
+		assertFalse(owner.validateBookingSummaryDate(LocalDate.now().minusMonths(1).minusDays(1)));
+	}
+
+}
