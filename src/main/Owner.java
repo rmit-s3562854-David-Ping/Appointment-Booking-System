6311869cd69660bc56dd02719ef8806ef75707a8
rmit-s3599135-hassenmender <hassen@192.168.1.11>
2017-04-06 20:31:38 +1000

    "comments added"

--- src/main/Owner.java
+++ src/main/Owner.java
@@ -41,7 +41,11 @@ public class Owner extends Member {
 	}
 
 	/**
-	 * 
+	 * get the new employee's info
+	 * then create an employee object and return it
+	 * if any of the info is null the employee object is null
+	 * and creation fails But if all info is provided
+	 * employee created successfully
 	 * @author Hassan Mender
 	 */
 	public Boolean createEmployee() {
@@ -95,6 +99,7 @@ public class Owner extends Member {
 
 		Utility util = new Utility();
 		String id = util.createID();
+		System.out.println("Employee id: " + id);
 		System.out.println("Employee First Name: ");
 		String firstName = input.nextLine();
 		if (util.quitFunction(firstName)) {
@@ -125,7 +130,8 @@ public class Owner extends Member {
 	}
 
 	/**
-	 * 
+	 * times are null because they are added in a different method
+	 * new employees dont have any work times assigned
 	 * @author Hassan Mender
 	 */
 	public Employee makeEmployeeObj(String firstName, String lastName, String id) {
@@ -146,7 +152,9 @@ public class Owner extends Member {
 	}
 
 	/**
-	 * 
+	 * prompt the owner with employee Id 
+	 * If employee exists in the System confirm request
+	 * if confirmed remove employee from system and from file
 	 * @author Hassan Mender
 	 */
 	public Boolean deleteEmployee() {
