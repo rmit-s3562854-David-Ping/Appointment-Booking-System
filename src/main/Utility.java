dc86caaaa8d499c525119886e83f5e2f9ee80a2c
rmit-s3599135-hassenmender <hassen@192.168.1.11>
2017-04-06 20:35:52 +1000

    "comments added"

--- src/main/Utility.java
+++ src/main/Utility.java
@@ -1,185 +1,419 @@
 package main;
 
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Utility {
-	
-	//exit function to save all data to .txt files so it could be retrieved
-	//when program starts
-	public void exit(){
+
+	/**
+	 * Utility class, which contains all validation methods as well as other
+	 * functions for checking errors and allowing users to exit current
+	 * activity, etc
+	 * @version 1.00 05 Apr 2017
+	 * @author David Ping, Hassan Mender, Luke Waldren
+	 */
+
+	private static final Logger LOGGER = Logger.getLogger("MyLog");
+
+	/**
+	 * exit function to save all data to .txt files so it could be retrieved
+	 * when program starts
+	 * @author Hassan Mender
+	 * */
+	public void exit() {
 		Writer writer = new Writer();
 		Owner owner = new Owner();
-		
-		try{
+		Main main = new Main();
+
+		try {
+			writer.save(main.getCustomerArray());
+			writer.saveAppointments(main.getAppointmentArray());
 			writer.saveEmployees(owner.getEmployeeArray());
-		}catch (IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, e.toString(), e);
 		}
-		
+
 	}
-	
-	//when user inputs 'q' for input quit task
-	public boolean quitFunction(String input){
+
+	/**
+	 * when user is prompt any input
+	 * if input is any of the strings b,q or quit 
+	 * exit the function and return to menu
+	 * @author Hassan Mender
+	 * */
+	public boolean quitFunction(String input) {
 		String pattern = "^b|q|quit| $";
-		String quit = "q";
-		//System.out.println("quit function here");
 		boolean exit = false;
-		if(input.matches(pattern))
-		{
-			//System.out.println("exit is true");
+		if (input.matches(pattern)) {
 			exit = true;
 		}
+		LOGGER.info("Program Exited.");
 		return exit;
 	}
-	
-	public boolean checkString(String string){
-		Scanner keyboard = new Scanner(System.in);
-		Utility util = new Utility();
-		
-		while((string == null) || (string.trim().isEmpty()))
-		{			
+
+	/**
+	 * check that the String entered isnt empty 
+	 * @author Hassan Mender
+	 * */
+	public boolean checkString(String string) {
+		while ((string == null) || (string.trim().isEmpty())) {
 			return false;
-		}	
+		}
 		return true;
 	}
-	
-	public boolean customerUsernameIsDuplicate(String username){
-		Scanner keyboard = new Scanner(System.in);
-		Main driver = new Main();
-		
-		// If username already exists
-		int index = 0;
-		Boolean duplicate = false;
-		
-			/*System.out.println("Username:");
-			username = keyboard.nextLine();*/
-			while (index < driver.getCustomerArray().size()) {
-				if (driver.getCustomerArray().get(index).getUsername().equals(username)) {
-					duplicate = true;
-					System.out.println("This username already exists, please try a different one");
-					index = 0;
-					break;
-				} else if (index == driver.getCustomerArray().size() - 1) {
-					duplicate = false;
-					System.out.println("Username is available");
-				}
-				index++;
+
+	/**
+	 * Checks if the username entered in the registration already exists,
+	 * returns true if it does and false if it doesn't Will check both customer
+	 * and owner array to avoid duplicate usernames
+	 * 
+	 * @author Hassan Mender, David Ping
+	 */
+	public boolean customerUsernameIsDuplicate(String username) {
+		Main main = new Main();
+		for (int i = 0; i < main.getOwnerArray().size(); i++) {
+			if (main.getOwnerArray().get(i).getUsername().equals(username)) {
+				System.out.println("This username already exists, please try a different one");
+				return true;
 			}
-		
-		return duplicate;
-		
+		}
+		for (int i = 0; i < main.getCustomerArray().size(); i++) {
+			if (main.getCustomerArray().get(i).getUsername().equals(username)) {
+				System.out.println("This username already exists, please try a different one");
+				return true;
+			}
+		}
+		System.out.println("Username is available");
+		return false;
 	}
-	
-	public boolean validateLogin(String usernameOrPassword){
-        String pattern = "^[a-zA-Z0-9-_]{6,20}$";
-        if(usernameOrPassword.matches(pattern)){
-            return true;
-        }else{
-        	System.out.println("invalid input, input has to be 6-20 letters");
-            return false;
-        }
-    }
-    
-    public boolean validateName(String name){
-        String pattern = "^[A-Z]{1}[a-z]{1,19}$";
-        if(name.matches(pattern)){
-            return true;
-        }else{
-        	System.out.println("please enter a valid name .i.e: David");
-            return false;
-        }
-    }
-    
-    public boolean validateContactNumber(String number){
-        String pattern = "^[(]?((0|[+]61)(2|4|3|7|8))?[)]?( |-)?[0-9]{2}( |-)?[0-9]{2}( |-)?[0-9]{1}( |-)?[0-9]{3}$";
-        if(number.matches(pattern)){
-            return true;
-        }else{
-        	System.out.println("please enter a valid phone number .i.e: 0412345610");
-            return false;
-        }
-    }
-    
-    public boolean validateMakeEmployeeId(String id){
-        Owner owner = new Owner();
-        String pattern = "^e[0-9]{5}$";
 
-        for(int i=0;i<owner.getEmployeeArray().size();i++){
-            if(owner.getEmployeeArray().get(i).getId().equals(id)&&id.matches(pattern)){
-                System.out.println("Id already exists");
-                return false;
-            }
-        }
-        if(id.matches(pattern)){
-            return true;
-        }
-        System.out.println("Id is invalid, please enter in the format eXXXXX (X=[0-9])");
-        return false;
-    }
-    
-    public boolean validateEmployeeId(String id){
-        Owner owner = new Owner();
-        String pattern = "^e[0-9]{5}$";
+	/**
+	 * Test username and password that is entered to ensure the length is
+	 * between 6-20 characters
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateLogin(String usernameOrPassword) {
+		String pattern = "^[a-zA-Z0-9-_]{6,20}$";
+		if (usernameOrPassword.matches(pattern)) {
+			return true;
+		} else {
+			System.out.println("invalid input, input has to be 6-20 letters");
+			return false;
+		}
+	}
 
-        for(int i=0;i<owner.getEmployeeArray().size();i++){
-            if(owner.getEmployeeArray().get(i).getId().equals(id)&&id.matches(pattern)){
-                if(id.matches(pattern)){
-                    return true;
-                }else{
-                	System.out.println("Id is invalid, please enter in the format eXXXXX (X=[0-9])");
-                }
-            }
-        }
-        System.out.println("Id does not exist");
-        return false;
-    }
-    
-    public boolean validateAddress(String address){
-        String pattern = "^[0-9a-zA-Z0-9_ ]{5,45}$";
-        if(address.matches(pattern)){
-            return true;
-        }else{
-        	System.out.println("please enter valid Address");
-            return false;
-        }
-    }
-    
-    public boolean validateDate(String date){
-    	LocalDate currentDate;
-    	DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
-    	try{
-    		currentDate = LocalDate.parse(date, dateFormat);
-    	}catch(Exception e){
-    		return false;
-    	}
-    	return true;
-    }
-    
-    public boolean validateTime(String time){
-    	LocalTime currentTime;
-    	boolean valid =false;
-    	DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("H:mm");
+	/**
+	 * Test for names, to ensure name starts with a capital and is between 2-20
+	 * characters in length
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateName(String name) {
+		String pattern = "^[A-Z]{1}[a-z]{1,19}$";
+		if (name.matches(pattern)) {
+			return true;
+		} else {
+			System.out.println("please enter a valid name .i.e: David");
+			return false;
+		}
+	}
+
+	/**
+	 * Validates the contact number entered to ensure it adheres to Australian
+	 * phone number conventions
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateContactNumber(String number) {
+		String pattern = "^[(]?((0|[+]61)(2|4|3|7|8))?[)]?( |-)?[0-9]{2}( |-)?[0-9]{2}( |-)?[0-9]{1}( |-)?[0-9]{3}$";
+		if (number.matches(pattern)) {
+			return true;
+		} else {
+			System.out.println("please enter a valid phone number .i.e: 0412345610");
+			return false;
+		}
+	}
+
+	/**
+	 * Validates employee ID which must start with an 'e' and be followed by 5
+	 * digits, also checks if the id exists in the employee array
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateEmployeeId(String id) {
+		Owner owner = new Owner();
+		String pattern = "^e[0-9]{5}$";
+
+		for (int i = 0; i < owner.getEmployeeArray().size(); i++) {
+			if (owner.getEmployeeArray().get(i).getId().equals(id) && id.matches(pattern)) {
+				if (id.matches(pattern)) {
+					return true;
+				} else {
+					System.out.println("Id is invalid, please enter in the format eXXXXX (X=[0-9])");
+				}
+			}
+		}
+		System.out.println("Id does not exist");
+		return false;
+	}
+
+	/**
+	 * Validates address input, not very specific due to the nature of
+	 * addresses, address can contain digits, letters (lowercase and uppercase)
+	 * and _. It must be between 5-45 characters in length
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateAddress(String address) {
+		String pattern = "^[0-9a-zA-Z0-9_ ]{5,45}$";
+		if (address.matches(pattern)) {
+			return true;
+		} else {
+			System.out.println("please enter valid Address");
+			return false;
+		}
+	}
+
+	/**
+	 * Will test if the users input is in the correct format when entering a
+	 * date, will return false if it catches an exception
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateDate(String date) {
+		LocalDate currentDate;
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("d/M/yyyy");
+		try {
+			currentDate = LocalDate.parse(date, dateFormat);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Will test if the input adheres to one of 2 formats allowed either 14:00
+	 * (24 hour time) or 2:00PM (12 hour time) function will return true if it
+	 * is one of these 2 formats
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateTime(String time) {
+		LocalTime currentTime;
+		boolean valid = false;
+		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("H:mm");
 		DateTimeFormatter timeFormat2 = DateTimeFormatter.ofPattern("h:mma");
-		
-    	try{
-    		currentTime = LocalTime.parse(time, timeFormat);
-    		valid = true;
-    	}catch(Exception e){}
-    	
-    	try{
-    		currentTime = LocalTime.parse(time, timeFormat2);
-    		valid = true;
-    	}catch(Exception e){}
-    	
-    	if(valid==true){
-    		return true;
-    	}
-    	return false;
-    }
 
+		try {
+			currentTime = LocalTime.parse(time, timeFormat);
+			valid = true;
+		} catch (Exception e) {
+		}
+
+		try {
+			currentTime = LocalTime.parse(time, timeFormat2);
+			valid = true;
+		} catch (Exception e) {
+		}
+
+		if (valid == true) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * This method creates a unique ID in the format eXXXXX (X being a number).
+	 * 
+	 * @author Luke Waldren
+	 */
+	public String createID() {
+		Owner owner = new Owner();
+		ArrayList<Employee> EmployeeArray = owner.getEmployeeArray();
+		int IDCounter = 1;
+		int index = 0;
+		while (index < EmployeeArray.size()) {
+			index = 0;
+			while (index < EmployeeArray.size()) {
+				if (Integer.parseInt(String.format("%05d", IDCounter)) == Integer
+						.parseInt((EmployeeArray.get(index).getId()).substring(1, 6))) {
+					IDCounter++;
+					break;
+				} else {
+					index++;
+				}
+			}
+		}
+		String ID = "e" + String.format("%05d", IDCounter);
+		return ID;
+	}
+
+	/**
+	 * Checks whether or not the input is a valid day where the business is open
+	 * returns true if yes and false if not open, uses DayOfWeek values
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateDayOfWeek(LocalDateTime currentTime) {
+		Business business = new Business();
+		for (int i = 0; i < business.getOpeningDays().length; i++) {
+			if (business.getOpeningDays()[i].equals(currentTime.getDayOfWeek())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Tests whether or not the input is valid and the business is open, this
+	 * one takes an integer input where 1= MONDAY .... 7 = SUNDAY
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateDayOfWeek(int counter) {
+		Business business = new Business();
+		for (int i = 0; i < business.getOpeningDays().length; i++) {
+			if (business.getOpeningDays()[i].equals(DayOfWeek.of(counter))) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Tests the input and checks which of the two formats it adheres to, will return that format
+	 * @author David Ping
+	 * */
+	public DateTimeFormatter assignTimeFormat(String time) {
+		LocalTime currentTime;
+		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("H:mm");
+		DateTimeFormatter timeFormat2 = DateTimeFormatter.ofPattern("h:mma");
+
+		try {
+			currentTime = LocalTime.parse(time, timeFormat);
+			return timeFormat;
+		} catch (Exception e) {
+		}
+
+		try {
+			currentTime = LocalTime.parse(time, timeFormat2);
+			return timeFormat2;
+		} catch (Exception e) {
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Tests whether or not the LocalTime values entered are valid by ensuring
+	 * that 1. the start time is before the end time 2. the start time is after
+	 * the business opens 3. the end time is is before the business closes 4.
+	 * the distance between the 2 times is at least 3 hours (assuming min work
+	 * hours is 3)
+	 * 
+	 * @author David Ping
+	 */
+	public boolean validateNewWorkTime(LocalTime startTime, LocalTime endTime) {
+		Business business = new Business();
+		if (startTime.compareTo(endTime) > 0) {
+			System.out.println("end time is before start time");
+			return false;
+		} else if (startTime.isBefore(business.getOpeningTime())) {
+			System.out.println("cannot start before business is open");
+			return false;
+		} else if (endTime.isAfter(business.getClosingTime())) {
+			System.out.println("cannot have end after business is closed");
+			return false;
+		} else if (endTime.getHour() - startTime.getHour() < 3) {
+			System.out.println("cannot work for less than 3 hours");
+			return false;
+		} else if (endTime.getHour() - startTime.getHour() == 3 && startTime.getMinute() > endTime.getMinute()) {
+			System.out.println("cannot work for less than 3 hours");
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * An insertion sort method that that returns the appointments over the next
+	 * week in order of dates and times, used for viewing upcoming bookings
+	 * returns the sorted list
+	 * @author David Ping
+	 */
+	public List<Appointment> sortFutureAppointments() {
+		Main main = new Main();
+		LocalDateTime now = LocalDateTime.now();
+		List<Appointment> sortedList = new ArrayList<Appointment>();
+		for (int i = 0; i < main.getAppointmentArray().size(); i++) {
+			if (main.getAppointmentArray().get(i).getDateAndTime().isBefore(now.plusWeeks(1))
+					&& (main.getAppointmentArray().get(i).getDateAndTime().isAfter(now)
+							|| main.getAppointmentArray().get(i).getDateAndTime().equals(now))
+					&& sortedList.isEmpty()) {
+				sortedList.add(main.getAppointmentArray().get(i));
+				continue;
+			}
+			for (int j = 0; j < sortedList.size(); j++) {
+				if (main.getAppointmentArray().get(i).getDateAndTime().isBefore(now.plusWeeks(1))
+						&& (main.getAppointmentArray().get(i).getDateAndTime().isAfter(now)
+								|| main.getAppointmentArray().get(i).getDateAndTime().equals(now))
+						&& main.getAppointmentArray().get(i).getDateAndTime()
+								.isBefore(sortedList.get(j).getDateAndTime())) {
+					sortedList.add(j, main.getAppointmentArray().get(i));
+					break;
+				} else if (main.getAppointmentArray().get(i).getDateAndTime().isBefore(now.plusWeeks(1))
+						&& (main.getAppointmentArray().get(i).getDateAndTime().isAfter(now)
+								|| main.getAppointmentArray().get(i).getDateAndTime().equals(now))
+						&& j == sortedList.size() - 1) {
+					sortedList.add(main.getAppointmentArray().get(i));
+					break;
+				}
+			}
+		}
+		return sortedList;
+	}
+
+	/**
+	 * An insertion sort method that sorts all appointments in the past month and returns the sorted list
+	 * @author David Ping
+	 */
+	public List<Appointment> sortPastAppointments() {
+		Main main = new Main();
+		LocalDateTime now = LocalDateTime.now();
+		List<Appointment> sortedList = new ArrayList<Appointment>();
+		for (int i = 0; i < main.getAppointmentArray().size(); i++) {
+			if ((main.getAppointmentArray().get(i).getDateAndTime().isAfter(now.minusMonths(1))
+					|| main.getAppointmentArray().get(i).getDateAndTime().equals(now.minusMonths(1)))
+					&& main.getAppointmentArray().get(i).getDateAndTime().isBefore(now) && sortedList.isEmpty()) {
+				sortedList.add(main.getAppointmentArray().get(i));
+				continue;
+			}
+			for (int j = 0; j < sortedList.size(); j++) {
+				if (main.getAppointmentArray().get(i).getDateAndTime().isAfter(now.minusMonths(1))
+						&& main.getAppointmentArray().get(i).getDateAndTime().isBefore(now)
+						&& main.getAppointmentArray().get(i).getDateAndTime()
+								.isBefore(sortedList.get(j).getDateAndTime())) {
+					sortedList.add(j, main.getAppointmentArray().get(i));
+					break;
+				} else if (main.getAppointmentArray().get(i).getDateAndTime().isAfter(now.minusMonths(1))
+						&& main.getAppointmentArray().get(i).getDateAndTime().isBefore(now)
+						&& j == sortedList.size() - 1) {
+					sortedList.add(main.getAppointmentArray().get(i));
+					break;
+				}
+			}
+		}
+		return sortedList;
+	}
 }
