744342839ac1d88f8551e0ea12e5341641c20d8e
rmit-s3599135-hassenmender <s3599135@student.rmit.edu.au>
2017-04-07 22:37:08 +1000

    Update Customer_Test.java

--- src/JunitTest/Customer_Test.java
+++ src/JunitTest/Customer_Test.java
@@ -10,6 +10,18 @@ import main.Customer;
 import main.Main;
 import main.Utility;
 
+/**
+ * customer Junit tests
+ * test the customer login method and that it accepts the right username and password
+ * otherwise it will return false as non acceptance for entries 
+ * 
+ * test the registeration method 
+ * the individual validation tests are done separately in a different Junit class
+ * registerCustomer method checks that the registration fields are all valid to create a customer
+ * if any of the fileds is invalid the registration isn't successfull
+ * a boolean value is passed to makeObj if fields are valid or not 
+ * according to the boolean value the customer is created
+ */
 
 public class Customer_Test {
 
